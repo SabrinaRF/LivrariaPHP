740761cf0e49a792b4f77172f85091300140398d
sabrina <sabrinarfernandes.srf@gmail.com>
2023-03-03 18:23:33 -0300

    Interface publicacao

---
+++ Publicacao.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *
+ * @author sabri
+ */
+interface Publicacao {
+    public function abrir();
+    public function fechar();
+    public function folhear();
+    public function avancarPag();
+    public function voltarPag();
+}
